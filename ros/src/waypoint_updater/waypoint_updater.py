31459d07a3565092c25db4fb60a48cc124426ba8
vishalrangras <vishalrangras@yahoo.com>
2018-03-21 19:44:50 +0530

    Commit for Partial Waypoint Updater.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, TwistStamped
 from styx_msgs.msg import Lane, Waypoint
+import math, tf
 
-import math
+import copy
+from std_msgs.msg import Int32
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -37,16 +39,87 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.car_x = None
+        self.car_y = None
+        self.car_z = None
 
+        self.orientation = None
+
+        self.car_roll = None
+        self.car_pitch = None
+        self.car_yaw = None
+
+        #self.frame_id = None
+        #self.msg_seq = 1
+
+        self.waypoints = None
+        self.final_waypoints = Lane()
+        self.no_waypoints = None
+
+        self.closest = None #May remove it later
+        
         rospy.spin()
+
+    def get_closest_waypoint_index(self):
+        if self.car_x is None or self.car_y is None or self.car_z is None or self.waypoints is None:
+            return None
+        distance = 1E10
+        closest_waypoint_index = -1000
+        for index, waypoint in enumerate(self.waypoints):
+            waypoint_distance = math.sqrt((self.car_x - wp.pose.pose.position.x)**2 +\
+                                          (self.car_y - wp.pose.pose.position.y)**2 +\
+                                          (self.car_z - wp.pose.pose.position.z)**2)
+
+            if waypoint_distance < distance:
+                distance = waypoint_distance
+                closest_waypoint_index = index
+        return index
+
+    def get_local_coordinates(self, waypoint):
+        x, y = waypoint.pose.pose.position.x - self.car_x, waypoint.pose.pose.position.y - self.car_y
+        x_local = math.cos(self.car_yaw) * x + math.sin(self.car_yaw) * y
+        y_local = -math.sin(self.car_yaw) * x + math.cos(self.car_yaw) * y
+        return x_local, y_local
+
+    def get_next_waypoints(self):
+        closest_waypoint_index = self.get_closest_waypoint_index()
+
+        if self.car_yaw is None or self.waypoints is None or closest_waypoint_index is None:
+            return None
+
+        waypoint = self.waypoints[closest_waypoint_index]
+        x_local, y_local = self.get_local_coordinates(waypoint)
+
+        if x_local < 0:
+            closest_waypoint_index = (closest_waypoint_index + 1) % self.no_waypoints
+
+        lane = Lane()
+        for i in range(LOOKAHEAD_WPS):
+            lane.waypoints.append(self.waypoints[(closest_waypoint_index + i) % self.no_waypoints])
+
+        return lane
+
 
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
+        self.car_x = msg.pose.position.x
+        self.car_y = msg.pose.position.y
+        self.car_z = msg.pose.position.z
+
+        self.orientation = msg.pose.orientation
+        quaternion = [self.orientation.x, self.orientation.y, self.orientation.z, self.orientation.w]
+        self.car_roll, self.car_pitch, self.car_yaw = tf.transformations.euler_from_quaternion(quaternion)
+
+        if self.final_waypoints is not None:
+            self.final_waypoints_pub.publish(self.final_waypoints)
+        else:
+            self.final_waypoints = self.get_next_waypoints()
+            self.final_waypoints_pub.publish(self.final_waypoints)
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
-        pass
+        self.waypoints = msg.waypoints
+        self.no_waypoints = len(mgs.waypoints)
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -69,6 +142,8 @@ class WaypointUpdater(object):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
+
+
 
 
 if __name__ == '__main__':
